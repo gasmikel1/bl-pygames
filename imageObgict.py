7a3f349d71ce47be6b93d1dff444b434a1b664f8
gasmikel1 <mgaskins845@boyslatin.net>
2025-03-20 10:25:52 -0400

    ImageTest

---
+++ imageObgict.py
@@ -0,0 +1,20 @@
+
+
+
+
+# importing required library
+import pygame
+import sys
+
+screen=pygame.display.set_mode((600,250))
+pygame.display.set_caption("perfect")
+perfect_rect= perfect.get_rect()
+while True:
+    for event in pygame.event.get():
+        if event.typ==pygame.QUIT()
+        pygame.quit()
+        sys.exit()
+    
+    screen.fill("white")
+
+pygame.display.update()
